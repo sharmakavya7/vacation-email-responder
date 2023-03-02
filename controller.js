ce8042e2b830e471cc2d8f5b16ae63d845cee78b
sharmakavya7 <sharmakavya1002@gmail.com>
2023-03-03 05:06:25 +0530

    Final changes

--- controller.js
+++ controller.js
@@ -1,11 +1,11 @@
 const logic = require('./logic');
 
 function repeat(req, res) {
-    const email = req.params.email;
-    logic.getMessage(email);
-    res.send("people have been notified about us being on a vacation");
-  }
+  const email = req.params.email;
+  logic.getMessage(email);
+  res.send("People have been notified about you being on a vacation");
+}
   
-  module.exports = {
-    repeat
-  };
+module.exports = {
+  repeat
+};
